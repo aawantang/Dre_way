7ea9ab033f3dd873a3f38d2a72ae7caef3d8b7d5
andrewawantang <andrewawantang@10.242.1.8>
2019-09-05 12:15:53 -0400

    Project all done

---
+++ class 1/src/hellothere.java
@@ -0,0 +1,27 @@
+
+public class hellothere {
+
+	public static void main(String[] args) {
+		// project name 
+
+	/*System.out.println("Andrew Awantang:");
+	System.out.print(" today");
+	System.out.print(" is");
+	System.out.print(" September");
+	System.out.print(" 4th");
+	System.out.print(" 2019");
+	System.out.println(" Mostly cloudy");
+	System.out.print(" 10% ");
+	System.out.print(" chance");
+	System.out.print(" of rain");*/
+		
+		System.out.print("Hello" + " World");
+		System.out.println(" The Value is: " + 5);
+		System.out.println("list of movies i enjoy:\n");
+		System.out.print("\tWarrior\n");
+		System.out.print("\tThe Great Gatsbey\n");
+		
+
+	}
+
+}
